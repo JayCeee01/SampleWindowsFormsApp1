383a93ac0748c3e6c5bc9a14b4fef2a0ae57aabb
JayCee <158656310+JayCeee01@users.noreply.github.com>
2024-10-13 16:00:28 +0800

    Add project files.

---
+++ SampleWindowsFormsApp1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleWindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Visible = false;
+            label5.Visible = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string full_name, section;
+                double math, filipino, english, history, pe, total_average;
+
+                full_name = textBox7.Text;
+                section = textBox6.Text;
+                math = double.Parse(textBox2.Text);
+                english = double.Parse(textBox1.Text);
+                history = double.Parse(textBox3.Text);
+                filipino = double.Parse(textBox4.Text);
+                pe = double.Parse(textBox5.Text);
+
+                total_average = (math + english + history + filipino + pe) / 5;
+
+                label4.Visible = true;
+                label5.Visible = true;
+
+                label4.Text = $"{full_name} from {section} has an average grade is {total_average}" ;
+                label5.Text = total_average > 75 ? "You passed this semester" : "You failed this semester";
+            }
+            catch (FormatException )
+            {
+
+                MessageBox.Show("An error has occured. Please fill out all the boxes with the correct input.");
+            }
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
