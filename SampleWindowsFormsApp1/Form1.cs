d27fbdb6c69699e97f96d1b755d27a749f4106e6
JayCee <158656310+JayCeee01@users.noreply.github.com>
2024-10-13 16:25:27 +0800

    Revert "Update Form1.cs"
    This reverts commit b4f98075ac1ef037a73e85ce3929e605af94ecad.

--- SampleWindowsFormsApp1/Form1.cs
+++ SampleWindowsFormsApp1/Form1.cs
@@ -17,7 +17,7 @@ namespace SampleWindowsFormsApp1
             InitializeComponent();
             label4.Visible = false;
             label5.Visible = false;
-            label12.Text = "This is an overwrite change in label, This will be a test for fetching";
+            label12.Text = "This is a overwrite change in label";
 
         }
 
